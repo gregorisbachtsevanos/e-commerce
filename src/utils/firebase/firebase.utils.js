089b12d614c4f99b89a0de30ed0b4397d4c6ce9a
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-08-29 17:37:55 +0300

    firebase init

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+   getAuth,
+   signInWithRedirect,
+   signInWithPopup,
+   GoogleAuthProvider,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: "AIzaSyBDdK6y_VizNoUnTi7TSN3gvpJeveRfCC8",
+   authDomain: "e-store-app-e6007.firebaseapp.com",
+   projectId: "e-store-app-e6007",
+   storageBucket: "e-store-app-e6007.appspot.com",
+   messagingSenderId: "13537068159",
+   appId: "1:13537068159:web:427f432d68e3bb92026b4a",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+   prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithGooglePopup(auth, provider)
