593b290a60bf772a291dda21a1570f41ac55fbbe
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-08-30 14:07:02 +0300

    firebase auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,6 +7,13 @@ import {
    signInWithPopup,
    GoogleAuthProvider,
 } from "firebase/auth";
+
+import { 
+   getFirestore,
+   doc,
+   getDoc,
+   setDoc
+} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -28,4 +34,31 @@ provider.setCustomParameters({
 })
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithGooglePopup(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth)  => {
+   const userDocRef = doc(db, 'users', userAuth.uid);
+
+   console.log(userDocRef);
+   const userSnapshot = await getDoc(userDocRef)
+   console.log(userSnapshot.exists());
+
+   if(!userSnapshot.exists()) {
+      const { displayName, email} = userAuth;
+      const createdAt = new Date();
+      try {
+         await setDoc(userDocRef, {
+            displayName,
+            email,
+            createdAt
+         })
+      } catch (error) {
+         console.error(error)
+      }
+   }else{
+      return userDocRef
+   }
+
+}
